d9ff84a13576e8f0ec511c1bbc96ef65ccaaa2ff
avitran0 <holyhades64@gmail.com>
2026-03-04 11:18:05 +0100

    gl

--- src/library/sdl.rs
+++ src/library/sdl.rs
@@ -5,6 +5,7 @@ use crate::{interop::cstr, library::Library};
 
 /// void SDL_GL_SwapWindow(SDL_Window *)
 type GlSwapFn = extern "C" fn(*mut c_void);
+type GLGetProcAddressFn = extern "C" fn(*const c_char) -> *mut c_void;
 /// int SDL_PollEvent(SDL_Event *)
 type PollEventFn = extern "C" fn(*mut c_void) -> c_int;
 /// int SDL_ShowSimpleMessageBox(Uint32 flags, const char *title, const char *message, SDL_Window *window)
@@ -13,6 +14,7 @@ type ShowSimpleMessageBoxFn = extern "C" fn(u32, *const c_char, *const c_char, *
 pub struct SDL {
     library: Library,
     gl_swap_fn: GlSwapFn,
+    gl_get_proc_address_fn: GLGetProcAddressFn,
     message_box_fn: ShowSimpleMessageBoxFn,
 }
 
@@ -20,13 +22,21 @@ impl SDL {
     pub fn new() -> Option<Self> {
         let library = Library::new("libSDL2-2.0.so.0")?;
         let gl_swap_fn = library.function("SDL_GL_SwapWindow")?.cast();
+        let gl_get_proc_address_fn = library.function("SDL_GL_GetProcAddress")?.cast();
         let message_box_fn = library.function("SDL_ShowSimpleMessageBox")?.cast();
 
         Some(Self {
             library,
+            gl_get_proc_address_fn,
             gl_swap_fn,
             message_box_fn,
         })
+    }
+
+    pub fn gl(&self) -> glow::Context {
+        unsafe {
+            glow::Context::from_loader_function_cstr(|s| (self.gl_get_proc_address_fn)(s.as_ptr()))
+        }
     }
 
     pub fn message_box(&self, kind: MessageBoxKind, title: &str, message: &str) {
