a8b800aa553f24fdba273e8503e4601ec24a5ce8
avitran0 <holyhades64@gmail.com>
2026-03-05 21:27:30 +0100

    more engine interface funcs

--- src/interface/engine.rs
+++ src/interface/engine.rs
@@ -19,4 +19,23 @@ impl EngineInterface {
         func(self.interface.handle(), &mut width, &mut height);
         (width, height)
     }
+
+    pub fn local_player_index(&self) -> i32 {
+        type LocalPlayerIndexFn = extern "C" fn(*const c_void) -> i32;
+        let func: LocalPlayerIndexFn = self.interface.vfunc(12);
+        func(self.interface.handle())
+    }
+
+    /// player is in game, not loading
+    pub fn in_game(&self) -> bool {
+        type InGameFn = extern "C" fn(*const c_void) -> bool;
+        let func: InGameFn = self.interface.vfunc(26);
+        func(self.interface.handle())
+    }
+
+    pub fn world_to_screen_matrix(&self) -> glam::Mat4 {
+        type WorldToScreenMatrixFn = extern "C" fn(*const c_void) -> *const glam::Mat4;
+        let func: WorldToScreenMatrixFn = self.interface.vfunc(37);
+        unsafe { *func(self.interface.handle()) }
+    }
 }
